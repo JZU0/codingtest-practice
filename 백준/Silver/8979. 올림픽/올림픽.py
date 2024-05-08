7419cdb095ca0721de63bbebd9a8667a610e42c4
이주현 <97423172+JZU0@users.noreply.github.com>
2024-05-08 11:30:14 +0900

    [Silver V] Title: 올림픽, Time: 140 ms, Memory: 110272 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,26 +1,12 @@
-N, K = map(int,input().split())
+N, K = map(int, input().split())
 
-result = {}
-for _ in range(N):
-    line = input().split()  
-    key = int(line[0])  
-    values = list(map(int, line[1:]))  
-    result[key] = values  
+medals = [list(map(int, input().split())) for _ in range(N)]
+    
+medals.sort(key = lambda x : (x[1], x[2], x[3]), reverse=True)
 
-sorted_result = sorted(result.items(), key=lambda x: (x[1][0], x[1][1], x[1][2]), reverse=True)
+idx = [medals[i][0] for i in range(N)].index(K)
 
-rank = 1
-prevrank = 1
-afterrank = 1
-if(sorted_result[0][0] == K):
-    print(rank)
-else:
-    for i in range(1,N):
-        prevrank = rank
-        afterrank += 1
-        rank = afterrank
-        if(sorted_result[i][1] == sorted_result[i-1][1]):
-                rank = prevrank
-                afterrank += 1
-        if(sorted_result[i][0] == K):
-            print(rank)
+for i in range(N):
+    if medals[idx][1:] == medals[i][1:]:
+        print(i+1)
+        break
