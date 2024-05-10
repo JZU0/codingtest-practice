877e13b01fc245dbf30402362f785af5f59f0df8
이주현 <97423172+JZU0@users.noreply.github.com>
2024-05-10 10:30:27 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 644 ms, Memory: 309888 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -1,17 +1,28 @@
+# 시간 줄여보기
+# 딕셔너리 -> set으로 변경
+
 import sys
 
 S = sys.stdin.readline().strip()
 
-str_hash = {}
-ans = 0
+# str_hash = {}
+# ans = 0
+
+# for i in range(1, len(S)):
+#     for j in range(0, len(S)-i+1):
+#         s = S[j:j+i]
+#         if not s in str_hash:
+#          str_hash[s] = 1
+#          ans += 1
+
+# print(ans+1)
+
+ans = set()
 
 for i in range(1, len(S)):
     for j in range(0, len(S)-i+1):
-        s = S[j:j+i]
-        if not s in str_hash:
-         str_hash[s] = 1
+        sub_string = S[j:j+i]
+        if not sub_string in ans:
+            ans.add(sub_string)
 
-for k in str_hash:
-   ans += str_hash[k]
-
-print(ans+1)
+print(len(ans) + 1)
