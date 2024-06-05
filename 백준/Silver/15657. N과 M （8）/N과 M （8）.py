8c291c0cb94e327de2eaf6c7298cbcbb0bb9d9b9
이주현 <97423172+JZU0@users.noreply.github.com>
2024-06-05 09:26:56 +0900

    [Silver III] Title: N과 M (8), Time: 140 ms, Memory: 111740 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+num_arr = (list(map(int, input().split())))
+num_arr.sort()
+
+arr = []
+
+def backtracking():
+    if(len(arr) == M):
+        print(" ".join(map(str, arr)))
+        return
+    
+    for i in num_arr:
+        if not arr or (arr[-1] <= i):
+            arr.append(i)
+            backtracking()
+            arr.pop()
+
+backtracking()
