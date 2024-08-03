7593562550908e05785d27e1951f43d45ce71f72
이주현 <97423172+JZU0@users.noreply.github.com>
2024-08-03 10:20:56 +0900

    [level 3] Title: 네트워크, Time: 0.35 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 10.8 MB, 시간: 2.90 ms
+메모리: 10.2 MB, 시간: 0.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 03일 09:33:59
+2024년 08월 03일 10:20:54
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,22 +1,20 @@
 import sys
 sys.setrecursionlimit(int(1e9))
 
-def solution(n, computers): 
-    
-    tmp = []
+def solution(n, computers):
+
     answer = 0
-    
-    def dfs(start,cnt=0):
-        computers[start][start] = 0
+    visited = [False] * n
+
+    def dfs(start):
+        visited[start] = True
         for idx, c in enumerate(computers[start]):
-            if(c==1):
-                computers[start][idx] = 0
+            if c == 1 and not visited[idx]:
                 dfs(idx)
-            
+
     for i in range(n):
-        for j in range(n):
-            if(computers[i][j]==1):
-                dfs(i)
-                answer += 1
+        if not visited[i]:
+            dfs(i)
+            answer += 1
 
     return answer
