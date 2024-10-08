d66b2d08c5d089f8976748c2bf9cb66f6d7cbf9d
JZU0 <97423172+JZU0@users.noreply.github.com>
2024-10-08 15:50:34 +0900

    [level 2] Title: 땅따먹기, Time: 453.62 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,14 +1,9 @@
 def solution(land):
     n = len(land)
-    temp = [[0]*4 for _ in range(n+1)]
-    for i in range(0,n):
-        for j in [0,1,2,3]:
-            if(j==0):
-                temp[i][j] = land[i][j] + max(temp[i-1][1],temp[i-1][2],temp[i-1][3])
-            elif(j==1):
-                temp[i][j] = land[i][j] + max(temp[i-1][0],temp[i-1][2],temp[i-1][3])
-            elif(j==2):
-                temp[i][j] = land[i][j] + max(temp[i-1][0],temp[i-1][1],temp[i-1][3])
-            elif(j==3):
-                temp[i][j] = land[i][j] + max(temp[i-1][0],temp[i-1][1],temp[i-1][2]) 
+    temp = [[0]*4 for _ in range(n)]
+
+    for i in range(n):
+        for j in range(4):
+            temp[i][j] = land[i][j] + max(temp[i-1][k] for k in range(4) if k != j)
+
     return max(temp[n-1])
