7e7271d51def44720d0065ec674a2c1ac1862480
JZU0 <97423172+JZU0@users.noreply.github.com>
2024-11-10 23:50:09 +0900

    [level 2] Title: 할인 행사, Time: 73.92 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,30 +1,26 @@
+from collections import Counter
+
 def solution(want, number, discount):
-    arr = {}
-    temp = {}
-    for i in range(len(want)):
-        arr[want[i]] = number[i]
-    start = -1
-    end = 9
+    arr = Counter({want[i]: number[i] for i in range(len(want))})
+    temp = Counter(discount[:10]) 
     answer = 0
-    
-    for i in range(10):
-        if discount[i] not in temp:
-            temp[discount[i]] = 1
-        else:
-            temp[discount[i]] += 1
-        
+    start = 0
+    end = 10
+
     while end < len(discount):
-        if(arr==temp):
+        if arr == temp:
             answer += 1
+
+        temp[discount[start]] -= 1
+        if temp[discount[start]] == 0:
+            del temp[discount[start]]
+
+        temp[discount[end]] += 1
+
         start += 1
-        if discount[start] in temp:
-            temp[discount[start]] -= 1
-            if temp[discount[start]] == 0:
-                del temp[discount[start]]
         end += 1
-        if end < len(discount):  
-            if discount[end] not in temp:
-                temp[discount[end]] = 1
-            else:
-                temp[discount[end]] += 1
+
+    if arr == temp:
+        answer += 1
+
     return answer
